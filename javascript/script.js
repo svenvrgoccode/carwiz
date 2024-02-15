778f576ae62a408b84f5e4e935ec9ec14effdd33
Sven Vrgoč <98204258+svenvrgoccode@users.noreply.github.com>
2024-02-15 18:23:46 +0100

    select finished

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 let dropdownList = document.querySelectorAll('.select li a')
 const dropdownContent = document.querySelectorAll('.poslovnica-contact')
+let selectText = document.querySelectorAll('.select-text')
 
 dropdownList.forEach((item)=>{
     item.addEventListener('mouseenter',(e)=>{
@@ -12,9 +13,31 @@ dropdownList.forEach((item)=>{
         e.target.classList.add('active')
         const content = e.target.nextElementSibling
         const poslovnica = content.querySelector('.poslovnica-contact')
-
         poslovnica.classList.add('active')
+    })
+})
 
+
+selectText.forEach((selectTxt)=> {
+    selectTxt.addEventListener('click',(e)=>{
+        const clickedDropdown = e.target.nextElementSibling
+        const allDropdowns = document.querySelectorAll('.select.active')
+        const clickedSelectedText = e.currentTarget
+
+        selectText.forEach((text)=>{
+            if(text !== clickedSelectedText){
+                text.classList.remove('active')
+            }
+        })
+  
+        allDropdowns.forEach((dropdown)=>{
+            if(dropdown !== clickedDropdown){
+                dropdown.classList.remove('active')
+            }
+        })
+
+        selectTxt.classList.toggle('active')
+        clickedDropdown.classList.toggle('active')
 
     })
 })

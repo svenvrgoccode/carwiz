d00d5c748270925bf8cf25bde118bba0dc1bc3ca
Sven Vrgoč <98204258+svenvrgoccode@users.noreply.github.com>
2024-02-15 23:20:05 +0100

    dropdown finished

--- javascript/script.js
+++ javascript/script.js
@@ -1,43 +1,75 @@
-let dropdownList = document.querySelectorAll('.select li a')
-const dropdownContent = document.querySelectorAll('.poslovnica-contact')
-let selectText = document.querySelectorAll('.select-text')
+const dropdownLists = document.querySelectorAll('.select li a')
+const dropdownContents = document.querySelectorAll('.poslovnica-contact')
+const selectTexts = document.querySelectorAll('.select-text')
+const selects = document.querySelectorAll('.select')
 
-dropdownList.forEach((item)=>{
-    item.addEventListener('mouseenter',(e)=>{
-        dropdownList.forEach((item) => {
-            item.classList.remove('active')
-        })
-        dropdownContent.forEach((content)=> {
-            content.classList.remove('active')
-        })
-        e.target.classList.add('active')
-        const content = e.target.nextElementSibling
-        const poslovnica = content.querySelector('.poslovnica-contact')
-        poslovnica.classList.add('active')
+
+function closeAllSelects() {
+    selects.forEach((select)=>{
+        select.classList.remove('active')
+    });
+    selectTexts.forEach((selectText) => {
+        selectText.classList.remove('active')
+    })
+}
+
+function clearList(){
+    dropdownLists.forEach((list)=>{
+        list.classList.remove('active')
+    })
+}
+
+function clearContact() {
+    dropdownContents.forEach((contact)=>{
+        contact.classList.remove('active')
+    })
+}
+
+
+selectTexts.forEach((selectText,index)=>{
+    selectText.addEventListener('click',(e)=>{
+        const listA = selects[index].querySelectorAll('.select li a')
+        const listPoslovnica = selects[index].querySelectorAll('.poslovnica-contact')
+        const firstPoslovnica = listPoslovnica[0]
+        const firstA = listA[0]
+
+        if(!selects[index].classList.contains('active')){
+
+            closeAllSelects();
+
+            selects[index].classList.toggle('active');
+            e.currentTarget.classList.toggle('active')
+
+            firstA.classList.add('active')
+            firstPoslovnica.classList.add('active')
+        }else{
+            selects[index].classList.remove('active');
+            e.currentTarget.classList.remove('active')
+            clearList()
+            clearContact()
+
+
+        }
+    })
+})
+
+dropdownLists.forEach((listItem,index)=>{
+    listItem.addEventListener('mouseenter',(e)=>{
+        clearList()
+        listItem.classList.add('active')
+        clearContact()
+        dropdownContents[index].classList.add('active')
     })
 })
 
 
-selectText.forEach((selectTxt)=> {
-    selectTxt.addEventListener('click',(e)=>{
-        const clickedDropdown = e.target.nextElementSibling
-        const allDropdowns = document.querySelectorAll('.select.active')
-        const clickedSelectedText = e.currentTarget
+document.addEventListener('click',(e)=> {
+    const isInsideSelect = Array.from(selects).some((select) => select.contains(e.target))
+    const isInsideSelectText = Array.from(selectTexts).some((selectText) => selectText.contains(e.target))
 
-        selectText.forEach((text)=>{
-            if(text !== clickedSelectedText){
-                text.classList.remove('active')
-            }
-        })
-  
-        allDropdowns.forEach((dropdown)=>{
-            if(dropdown !== clickedDropdown){
-                dropdown.classList.remove('active')
-            }
-        })
-
-        selectTxt.classList.toggle('active')
-        clickedDropdown.classList.toggle('active')
-
-    })
+    if(!isInsideSelect && !isInsideSelectText){
+        closeAllSelects();
+        clearList()
+        clearContact()
+    }
 })
